87b0af1e8dba92fa2baf87650767d450d50900ce
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-03-19 22:42:41 +0400

    Add logic that returns name instead of id

--- LibraryProject/src/main/java/com/example/LibraryProject/Servlets/BorrowServlet.java
+++ LibraryProject/src/main/java/com/example/LibraryProject/Servlets/BorrowServlet.java
@@ -7,6 +7,7 @@ import com.example.LibraryProject.Model.Borrowing;
 import com.example.LibraryProject.Model.Member;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,8 +15,10 @@ import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
+@WebServlet("/borrows")
 public class BorrowServlet extends HttpServlet {
     private final DatabaseConnectionManager dbcm = DatabaseConnectionManager.getInstance();
     private BorrowingsDAO borrowingsDAO = new BorrowingsDAO();
@@ -32,11 +35,40 @@ public class BorrowServlet extends HttpServlet {
         PrintWriter out = resp.getWriter();
 
         List<Borrowing> borrowings = borrowingsDAO.findAll();
-        objectMapper.writeValue(out, borrowings);
+        List<BorrowingCopy> borrowingCopies = new ArrayList<>();
+
+        for (Borrowing borrowing : borrowings) {
+            String name = new MembersDAO().findById(borrowing.getMemberId()).getName();
+
+            borrowingCopies.add(
+                    new BorrowingCopy(
+                            borrowing.getBookCode(),
+                            name,
+                            borrowing.getBorrowDate(),
+                            borrowing.getReturnDate()
+                    )
+            );
+        }
+
+        objectMapper.writeValue(out, borrowingCopies);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        String bookCode = req.getParameter("bookCode");
+        if("Add Borrowing".equals(action)) {
+            String email = req.getParameter("email");
 
+            Member member = new MembersDAO().findByEmail(email);
+            int id = member.getId();
+
+            borrowingsDAO.insert(
+                    new Borrowing(bookCode, id)
+            );
+        } else if ("Return Book".equals(action)) {
+            borrowingsDAO.update(bookCode);
+        }
+        resp.sendRedirect("/borrows.html");
     }
 }
