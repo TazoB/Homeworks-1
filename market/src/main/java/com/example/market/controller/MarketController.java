3542abea498eadfe74c7c083ae841ea06df80169
Tazo Bendianishvili <tazo.bendianishvili@gmail.com>
2026-05-13 16:03:30 +0400

    write necessary crud methods for controller class

---
+++ market/src/main/java/com/example/market/controller/MarketController.java
@@ -0,0 +1,47 @@
+package com.example.market.controller;
+
+import com.example.market.model.ItemRequestDTO;
+import com.example.market.model.ItemResponseDTO;
+import com.example.market.service.MarketService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/market")
+public class MarketController {
+    private final MarketService marketService;
+
+    public MarketController(MarketService marketService) {
+        this.marketService = marketService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemResponseDTO>> findAll() {
+        return ResponseEntity
+                .ok()
+                .body(marketService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemResponseDTO> saveItem(@RequestBody ItemRequestDTO item) {
+        return ResponseEntity
+                .ok()
+                .body(marketService.saveItem(item));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemResponseDTO> updateItem(@PathVariable int id, @RequestBody ItemRequestDTO item) {
+        return ResponseEntity
+                .ok()
+                .body(marketService.updateItem(id, item));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ItemResponseDTO> deleteItem(@PathVariable int id) {
+        return ResponseEntity
+                .ok()
+                .body(marketService.deleteItem(id));
+    }
+}
